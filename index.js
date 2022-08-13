d04cbb90ed2a41843fd78fdf6a13b469d204840c
cratuslinkr <111162223+cratuslinkr@users.noreply.github.com>
2022-08-13 09:35:55 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+const axios = require('axios');
+app.post("/", async(req,res) => {
+  try {
+  var body = req.body;
+  const data = (await axios(body)).data;
+  res.json(data);
+  }catch(e) {
+  res.json({error: `${e}`});
+  }
+})
+
+app.get('*', (req,res) => res.redirect(`https://github.com/codingstudios/Fetch-Relay`))
+
+app.listen(3000);
